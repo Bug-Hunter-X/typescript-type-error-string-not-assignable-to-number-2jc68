c19994af81c48d11111fd501d590acfb1cad0b8c
BugHunter-X <askzenai@gmail.com>
2024-12-05 01:16:07 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,13 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+let result = add(10, 20); // Corrected: Passing numbers
+console.log(result); 
+
+function addString(a: string, b: string): string {
+    return a + b
+}
+
+let stringResult = addString("hello", " world")
+console.log(stringResult) 
